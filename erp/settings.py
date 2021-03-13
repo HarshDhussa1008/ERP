c0895bb055b95dcbb6c716c0e8e3e2a07ba8ec6a
unknown <harshdhussa@gmail.com>
2021-03-13 22:14:57 +0530

    second commit

--- erp/settings.py
+++ erp/settings.py
@@ -15,6 +15,13 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -121,3 +128,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_URL= '/erp/shop_login/'
 LOGIN_REDIRECT_URL= '/erp/shop_login/'
+import django_heroku
+django_heroku.settings(locals())
